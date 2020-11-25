9b69b144493b2fb399b9956dfc92bb9d5495f446
Tsering Lama <tseringglamaa@gmail.com>
2020-11-24 20:40:06 -0500

    Initial Commit

---
+++ Nested-Orthogonal-Scrolling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nested-Orthogonal-Scrolling
+//
+//  Created by Tsering Lama on 11/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
